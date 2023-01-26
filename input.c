545962a0ddeef49b2a534c2e6132b47443a8c21f
Danial-Hosseintabar <danial.ht10@gmail.com>
2023-01-26 13:54:11 +0330

    removestr

--- input.c
+++ input.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_SIZE 100000
+
 void get_address(char string[MAX_SIZE]){
 	scanf("%c" , string ) ;
 	if(string[0] == '"'){
@@ -17,4 +19,30 @@ void get_address(char string[MAX_SIZE]){
 		scanf("%s" , string+1) ;
 	}
 
+}
+
+void readrest( char *str , FILE *fob){
+	int str_size = 0 ;
+	for(char c = fgetc(fob);c!=EOF; c = fgetc(fob)){
+		str[str_size] = c ;
+		str_size++ ;
+	}
+	str[str_size] = '\0' ;
+}
+
+void readto(char *str , FILE *fob , int line_pos , int char_pos ){
+	char c ;
+	int str_size = 0 ;
+	for(int line = 0 ; line < line_pos-1 ;){
+		c = fgetc(fob) ;
+		str[str_size] = c ;
+		str_size++ ;
+		if(c == '\n') line++ ;
+	}
+	for(int i = 0 ; i < char_pos ; i++){
+		c = fgetc(fob) ;
+		str[str_size] = c ;
+		str_size++ ;
+	}
+	str[str_size] = '\0' ;
 }
--- main.c
+++ main.c
@@ -2,19 +2,136 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "createfile.c"
 #include "input.c"
-#include "insertstr.c"
-#include "cat.c"
 
-#define MAX_SIZE 10000
+#define MAX_SIZE 100000
 
 int check_exit(char *command){
 	return !strcmp(command , "exit" ) ;
 }
 
-void get_text( char string[MAX_SIZE] ){
-	scanf("%s" , string) ;
+int check_createfile(char *command){
+	char opt[MAX_SIZE] ;
+	char add[MAX_SIZE] ;
+	if(strcmp(command , "createfile")) return 0 ;
+	scanf("%s" , opt) ;
+	if(strcmp("-file" , opt)) return 0 ;
+	getchar() ;
+	get_address(add) ;
+	printf("FILE CREATED SUCCESSFULLY : ( %s )\n" , add ) ;
+	FILE *fob = fopen(add , "a") ;
+	fclose(fob) ;
+	return 1 ;
+}
+
+int check_insertstr(char *command){
+	int line_pos ;
+	int char_pos ;
+	char file_name[MAX_SIZE] ;
+	char opt[MAX_SIZE] ;
+	char string[MAX_SIZE] ;
+
+	// no file found error ?
+
+	// GETTING INPUT
+	if(strcmp(command , "insertstr")) return 0 ;
+	scanf("%s" , opt ) ;
+	if(strcmp(opt , "-file")) return 0 ;
+	getchar() ;
+	get_address(file_name) ;
+	scanf("%s" , opt ) ;
+	if(strcmp(opt , "-str")) return 0 ;
+	getchar() ;
+	get_address(string) ;
+	scanf("%s" , opt ) ;
+	if(strcmp(opt , "-pos")) return 0 ;
+	getchar() ;
+	if(2!=scanf("%d:%d" , &line_pos , &char_pos)) return 0 ;
+
+	// debug : printf("file : %s , str : %s , line_pos : %d , char_pos : %d \n" , file_name , string , line_pos , char_pos ) ;
+
+	FILE *fob = fopen(file_name , "r+" ) ;
+
+	for(int i = 0 ; i < line_pos-1 ; i++){
+		char string_tmp[MAX_SIZE] ;
+		fgets(string_tmp , MAX_SIZE , fob) ;
+	}
+
+	fseek(fob , char_pos*sizeof(char) , SEEK_CUR) ;
+	fprintf(fob , "%s" , string ) ;
+	fclose(fob) ;
+
+	return 1 ;
+
+}
+
+int check_cat(char *command){
+	if(strcmp(command,"cat")) return 0 ;
+	char opt[MAX_SIZE] ;
+	char add[MAX_SIZE] ;
+	scanf("%s" , opt);
+	if(strcmp(opt,"-file")) return 0 ;
+	getchar() ;
+	get_address(add) ;
+
+	FILE *fob = fopen(add , "r") ;
+	char c = fgetc(fob);
+	while(c!=EOF){
+		printf("%c" , c) ;
+		c = fgetc(fob) ;
+	}
+	
+	printf("\n") ;
+	
+	fclose(fob) ;
+	return 1 ;
+}
+
+int check_removestr(char *command){
+	char file_name[MAX_SIZE] ;
+	char arg[MAX_SIZE] ;
+	int line_pos ;
+	int char_pos ;
+	int size ;
+	int mode ;
+
+	scanf("%s" , arg) ;
+	if(strcmp(arg,"-file")) return 0 ;
+	scanf("%s" , file_name);
+
+	scanf("%s" , arg) ;
+	if(strcmp(arg,"-pos")) return 0 ;
+	scanf("%d:%d" , &line_pos , &char_pos ) ;
+
+	scanf("%s" , arg) ;
+	if(strcmp(arg,"-size")) return 0 ;
+	scanf("%d" , &size) ;
+
+	scanf("%s" , arg) ;
+	if(!strcmp(arg , "-b")) mode = 1 ; // backward
+	else mode = 0 ; // forward
+
+	//forward
+	char text_pre[MAX_SIZE] ;
+	char text_post[MAX_SIZE] ;
+	FILE *fob = fopen(file_name , "r") ;
+	readto(text_pre , fob , line_pos , char_pos) ;
+	//fseek(fob , size , SEEK_CUR) ;
+	readrest(text_post , fob) ;
+	fclose(fob) ;
+	
+	if(mode){
+		for(int i = 0 ; i < size ; i++){
+			text_pre[strlen(text_pre)-1] = '\0' ;
+		}
+	}
+
+	fob = fopen(file_name , "w") ;
+	fprintf(fob , "%s%s" , text_pre , text_post+size*(1-mode) ) ;
+	fclose(fob) ;
+	return 1 ;
+
+
 }
 
 int main(){
@@ -27,26 +144,21 @@ int main(){
 
 		scanf("%s" , command) ;
 
-		// USE ARRAY !!!
-
-		if( check_exit(command) )
-			break ;
-
-		if( check_createfile(command) ){
-			command_run = 1 ;
+		if(!strcmp("exit",command)) return 0 ;
+		
+		int (*check_function[])(char*) = { check_createfile , check_insertstr , check_cat , check_removestr } ;
+		
+		for(int i = 0 ; i < 4 ; i++){
+			if( (*check_function[i])(command) ){
+				command_run = 1 ;
+			}
 		}
-		if( check_insertstr(command) ){
-			command_run = 1 ;
-		}
-
+		
 		if(command_run == 0){
+			char trash[MAX_SIZE] ;
+			gets(trash) ;
 			printf("Invalid input!\n") ;
 		}
 
 	}
-
-	printf("Press Enter to exit ...") ;
-	getchar() ;
 }
-
-
